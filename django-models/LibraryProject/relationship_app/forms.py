528d23b7e59a3e0201948e16c6caba792be087f0
unknown <nguimatsagrace123@gmail.com>
2025-11-16 17:42:07 -0800

    fix: Final definitive CustomUser model and settings push

--- django-models/LibraryProject/relationship_app/forms.py
+++
@@ -1,9 +0,0 @@
-# relationship_app/forms.py
-from django.contrib.auth.forms import UserCreationForm
-
-# We inherit from Django's built-in UserCreationForm for a standard registration form
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        # Use the default User model
-        fields = UserCreationForm.Meta.fields + ("email",) 
-        # Note: You can add more fields if needed, but 'email' is a common addition
