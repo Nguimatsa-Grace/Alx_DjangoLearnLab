0b0ab88142cd2a5ca92013480079455ac813e785
unknown <nguimatsagrace123@gmail.com>
2025-11-17 01:24:33 -0800

    FINAL CLEANUP: Untracked old, deleted directory paths

--- advance_feature_and_security/LibraryProject/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# Temporarily remove all registration to prevent startup crash
-# from django.contrib.auth.admin import UserAdmin
-# from .forms import CustomUserCreationForm, CustomUserChangeForm
-# from .models import CustomUser
-
-# @admin.register(CustomUser) 
-# class CustomUserAdmin(UserAdmin):
-#     ...
